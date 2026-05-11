aebd13f2395a241f737c7c516a01ea13bfc7a226
nguyenchuong07112009-hub <nguyenchuong07112009@gmail.com>
2026-05-11 22:30:09 +0700

    Create hello_world.cpp

---
+++ hello_world.cpp
@@ -0,0 +1,12 @@
+int main() {
+    int a, b;
+    cout << "Nhap so thu nhat: ";
+    cin >> a;
+    cout << "Nhap so thu nhat: ";
+    cin >> b;
+    
+    int sum = a + b;
+    cout << "Tong cua hai so la: " << sum << endl;
+    
+    return 0;
+}
